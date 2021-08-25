dfc395fc5669ea745d204db5e57a8f6d5c0ab7ce
shanmango <shanmarkshan@gmail.com>
2021-08-24 22:24:33 -0700

    mob schema done

--- backend/models/mob.js
+++ backend/models/mob.js
@@ -5,5 +5,30 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const mobSchema = new mongoose.Schema({
-  
+  id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true  
+  },
+  maps: {
+    type: [String],
+    required: true
+  },
+  drops: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Item'
+    }
+  ]
+
 })
+
+mobSchema.plugin(uniqueValidator)
+
+const Mob = mongoose.model('Mob', mobSchema)
+
+module.exports = Mob
