3f424b53ae391d9da68e254c4df08f572ce423b5
shanmango <shanmarkshan@gmail.com>
2021-08-25 03:20:05 -0700

    remove unnecessary endpoints on dev backend

--- parser/parser-backend/controllers/items.js
+++ parser/parser-backend/controllers/items.js
@@ -6,26 +6,11 @@ const itemsRouter = require('express').Router()
 const Item = require('../models/item')
 
 
-itemsRouter.get('/', async (req, res) => {
-  const items = await Item.find({})
-  res.json(items)
-})
-
-itemsRouter.get('/:id', async (req, res) => {
-  // Unique validation done in schema, game ID should be unique
-  const item = await Item.findOne({id: req.params.id})
-  if (item) {
-    res.json(item)
-  } else {
-    res.status(404).end()
-  }
-})
-
 itemsRouter.post('/', async (req, res) => {
   // Delete old data first
   await Item.collection.drop()
   const body = req.body
-  const response = await Item.insertMany(body)
+  await Item.insertMany(body)
   console.log('mass insertion of items complete')
   res.status(200).send('mass insertion of items complete')
 
--- parser/parser-backend/controllers/mobs.js
+++ parser/parser-backend/controllers/mobs.js
@@ -5,27 +5,12 @@
 const mobsRouter = require('express').Router()
 const Mob = require('../models/mob')
 
-mobsRouter.get('/', async (req, res) => {
-  const mobs = await Mob.find({})
-  res.json(mobs)
-})
-
-mobsRouter.get('/:id', async (req, res) => {
-  // Unique validation done in schema, game ID should be unique
-  const mob = await Mob.findOne({id: req.params.id})
-  if (mob) {
-    res.json(mob)
-  } else {
-    res.status(404).end()
-  }
-})
-
 mobsRouter.post('/', async (req, res) => {
   // Delete old data first
   await Mob.collection.drop()
   const body = req.body
   console.log(body)
-  const response = await Mob.insertMany(body)
+  await Mob.insertMany(body)
   console.log('mass insertion of mobs complete')
   res.status(200).send('mass insertion of mobs complete')
 })
