28c3df43da08fdac0311423d70cf1d0734b90796
shanmango <shanmarkshan@gmail.com>
2021-08-25 23:33:01 -0700

    use react-windowed-select instead of react-select

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,29 +1,6 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import Select, { createFilter } from 'react-select'
-import { FixedSizeList as List } from 'react-window'
-import { initializeItems } from '../reducers/itemReducer'
-
-/**
- * Custom MenuList to improve react-select performance
- */
-const MenuList = (props) => {
-  const height = 38
-  const { options, children, maxHeight, getValue } = props
-  const [value] = getValue()
-  const initialOffset = options.indexOf(value) * height
-
-  return (
-    <List
-      height={maxHeight / 3*2}
-      itemCount={children.length}
-      itemSize={height}
-      initialScrollOffset={initialOffset}
-    >
-      {({ index, style }) => <div style={style}>{children[index]}</div>}
-    </List>
-  )
-}
+import WindowedSelect, { createFilter } from 'react-windowed-select'
 
 /**
  * Search bar component
@@ -67,21 +44,16 @@ const Search = () => {
   const change = (event) => {
     console.log(event)
   }
-  const click = (event) => {
-    console.log('click')
-    console.log(event)
-  }
+
 
   return (
     <div style={style}>
-      <Select
+      <WindowedSelect
         style={style}
-        components={{ MenuList: MenuList }}
         isClearable
         filterOption={createFilter(filterConfig)}
         options={options}
-        onChange={change} 
-        onClick={click} />
+        onChange={change} />
     </div>
   )
 }
