534c2a4ef5a7d456d2d2d1229e94f6a0fb1f5472
shanmango <shanmarkshan@gmail.com>
2021-08-25 20:49:47 -0700

    call initializeMobs on load

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Mob from './components/Mob'
 import TypeSelector from './components/TypeSelector'
 import Search from './components/Search'
+import { useDispatch } from 'react-redux'
+import { initializeMobs } from './reducers/mobReducer'
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeMobs())
+  }, [dispatch])
+
   return (
     <div>
       <h1>MapleRoyals Drop Tracker</h1>
