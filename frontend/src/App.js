afefe4121e2a33501763bd39f2bbead8d8e4cd33
shanmango <shanmarkshan@gmail.com>
2021-08-26 17:12:24 -0700

    fronte nd ui changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,13 @@ function App() {
   return (
     <div>
       <h1>MapleRoyals Drop Tracker</h1>
-      Version: 73.4
+
+      <div style={{display: 'inline'}}>
+        Game Version: 73.4
+      </div>
+      <div style={{display: 'inline'}}>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Created By: Shanmango
+      </div>
       <hr></hr>
       <Search />
       <Info />
