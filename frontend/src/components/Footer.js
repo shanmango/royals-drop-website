4a42feaa40d55733cdadecf0920a029d6dd72c97
shanmango <shanmarkshan@gmail.com>
2021-08-30 23:01:43 -0700

    fix html error

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,10 +10,10 @@ const Footer = () => {
         <br />The drop data used for this website was taken from the
         Monster Book data in the Data folder of the MapleRoyals client.
         This website simply displays this data.
-        
+        </p>
+
         <p>Images were taken from <a href="https://maplestory.io">maplestory.io</a>
           <br />Created by: Shanmango
-        </p>
       </p>
     </div>
   )
