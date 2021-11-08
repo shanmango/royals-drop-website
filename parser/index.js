59b2bb4526d71fbf3fd9ba34f53155f8ad7d7e4f
shanmango <shanmarkshan@gmail.com>
2021-11-08 15:21:50 -0800

    generate json files

--- parser/index.js
+++ parser/index.js
@@ -6,7 +6,7 @@ const serverWriter = require('./src/serverWriter')
 
 // eslint-disable-next-line no-unused-vars
 const makeDataFile = (path, data) => {
-  fs.writeFile(path, JSON.stringify(data), (e) => {if (e) throw e})
+  fs.writeFile(path, JSON.stringify(data), (e) => { if (e) throw e })
 }
 
 const main = async () => {
@@ -19,7 +19,7 @@ const main = async () => {
   serverWriter.writeToServer(mobs, 'mobs')
   serverWriter.writeToServer(itemArray, 'items')
 
-  /*
+  
   makeDataFile('./jsondata/mobs.json', mobs)
   makeDataFile('./jsondata/items.json', items)
   makeDataFile('./jsondata/itemArray.json', itemArray)
@@ -27,7 +27,7 @@ const main = async () => {
   console.log('number of items dropped by mobs:', itemArray.filter(x => {
     return x.mobs.length > 0
   }).length)
-  */
   
+
 }
 main()
